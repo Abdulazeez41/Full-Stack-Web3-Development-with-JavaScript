d318a60778d5950ede79b32f5d6a957a049199c4
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2024-01-06 17:03:22 +0100

    Completed Fundme app using html

--- html-fund-me-fcc/index.js
+++ html-fund-me-fcc/index.js
@@ -2,9 +2,13 @@ import { ethers } from "./ethers-5.6.esm.min.js"
 import { abi, contractAddress } from "./constants.js"
 
 const connectButton = document.getElementById("connectButton")
-const fundButton = document.getElementById("fund")
+const fundButton = document.getElementById("fundButton")
+const balanceButton = document.getElementById("balanceButton")
+const withdrawButton = document.getElementById("withdrawButton")
 connectButton.onclick = connect
 fundButton.onclick = fund
+balanceButton.onclick = getBalance
+withdrawButton.onclick = withdraw
 
 async function connect() {
     if (typeof window.ethereum != "undefined") {
@@ -15,8 +19,16 @@ async function connect() {
     }
 }
 
+async function getBalance() {
+    if (typeof window.ethereum != "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const balance = await provider.getBalance(contractAddress)
+        console.log(ethers.utils.formatEther(balance))
+    }
+}
+
 async function fund() {
-    const ethAmount = "77"
+    const ethAmount = document.getElementById("ethAmount").value
     console.log(`Funding with ${ethAmount}...`)
     if (typeof window.ethereum != "undefined") {
         // provider / connection to the blockchain
@@ -41,15 +53,30 @@ async function fund() {
 function listenForTransactionMine(transactionResponse, provider) {
     console.log(`Mining ${transactionResponse.hash}...`)
     return new Promise((resolve, reject) => {
-       try {
-           provider.once(transactionResponse.hash, (transactionReceipt) => {
-               console.log(
-                   `Completed with ${transactionReceipt.confirmations} confirmations. `,
-               )
-               resolve()
-           })
-       } catch (error) {
-           reject(error)
-       }
+        try {
+            provider.once(transactionResponse.hash, (transactionReceipt) => {
+                console.log(
+                    `Completed with ${transactionReceipt.confirmations} confirmations. `,
+                )
+                resolve()
+            })
+        } catch (error) {
+            reject(error)
+        }
     })
+}
+
+async function withdraw() {
+    console.log(`Withdrawing...`)
+    if (typeof window.ethereum != "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.withdraw()
+            await listenForTransactionMine(transactionResponse, provider)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
